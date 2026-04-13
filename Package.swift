3b9f04936633eabbdad1c1503079ec7bd3f163ad
shangmaomao <shangmaomao@jasolar.com>
2026-04-13 16:08:15 +0800

    更新 package 描述文件，尝试解析

--- Package.swift
+++ Package.swift
@@ -20,6 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "YYImage",
+            path: "Sources",
+            sources: ["YYImage"],
             publicHeadersPath: "include",
             cSettings: [
                 .headerSearchPath("YYImage")
